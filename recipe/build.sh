63171a14b1423ccbb26b2f2caae099c0337c589e
Jack Olivieri <jolivieri@anaconda.com>
2024-04-03 15:57:05 +0200

    update build scripts

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,14 +1,30 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -ex
 
 if [[ "${target_platform}" == "linux-64" ]]; then
-    # similar to settings upstream in polars
-    export RUSTFLAGS='-C target-feature=+fxsr,+sse,+sse2,+sse3,+ssse3,+sse4.1,+sse4.2,+popcnt,+avx,+fma'
+  # similar to settings upstream in polars
+  export RUSTFLAGS='-C target-feature=+fxsr,+sse,+sse2,+sse3,+ssse3,+sse4.1,+sse4.2,+popcnt,+avx,+fma'
 fi
 
-# Run the maturin build via pip which works for direct and
-# cross-compiled builds.
-$PYTHON -m pip install . -vv --no-deps --no-build-isolation --ignore-installed --no-cache-dir
+echo rustc --version
 
-cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+if [[ "${build_platform}" == "linux-64") ]]; then
+  # we need to add the generate-import-lib feature since otherwise
+  # maturin will expect libpython DSOs at PYO3_CROSS_LIB_DIR
+  # which we don't have since we are not able to add python as a host dependency
+  cargo add pyo3 \
+      --manifest-path py-polars/Cargo.toml \
+      --features "abi3-py38,extension-module,multiple-pymethods,generate-import-lib"
+  maturin build --release --strip
+  pip install target/wheels/polars*.whl --target $PREFIX/lib/site-packages --platform win_amd64
+else
+  # Run the maturin build via pip which works for direct and
+  # cross-compiled builds.
+  $PYTHON -m pip install . -vv --no-deps --no-build-isolation --ignore-installed --no-cache-dir
+fi
+
+# The root level Cargo.toml is part of an incomplete workspace
+# we need to use the manifest inside the py-polars
+cd py-polars
+cargo-bundle-licenses --format yaml --output ../THIRDPARTY.yml
